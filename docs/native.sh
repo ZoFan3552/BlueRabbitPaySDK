49881b4c8e03063474c8d51c40ecc842eaa552d4
zeddic <zjf103552@outlook.com>
2024-05-23 18:44:04 +0800

    feat:Api test

---
+++ docs/native.sh
@@ -0,0 +1,3 @@
+curl -X POST https://api.ltzf.cn/api/wxpay/native \
+  -H "Content-Type: application/x-www-form-urlencoded" \
+  -d "mch_id=1677728047&out_trade_no=Zeddic20240523&total_fee=0.01&body=QQ公仔&timestamp=1716460770&notify_url=https://chat.zeddic.icu&sign=82447279912D8DAA8F7FB26B37FE8601"
--- src/test/java/com/zeddic/test/ApiTest.java
+++ src/test/java/com/zeddic/test/ApiTest.java
@@ -1,7 +1,23 @@
 package com.zeddic.test;
 
+import com.zeddic.util.SignUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ApiTest {
     public static void main(String[] args) {
-        System.out.println("zeddic test");
+        long timestamp = System.currentTimeMillis() / 1000;
+        System.out.println(timestamp);
+        Map<String , String> dataMap = new HashMap<>();
+        dataMap.put("mch_id" , "1677728047");
+        dataMap.put("out_trade_no" , "Zeddic20240523");
+        dataMap.put("total_fee" , "0.01");
+        dataMap.put("body" , "QQ公仔");
+        dataMap.put("timestamp" , String.valueOf(timestamp));
+        dataMap.put("notify_url" , "https://chat.zeddic.icu");
+        String sign = SignUtils.createSign(dataMap, "cd43230d2720924143ccdfd1e83f4cdb");
+        System.out.println(sign);
+
     }
 }
