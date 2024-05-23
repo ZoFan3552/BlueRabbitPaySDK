a1e102b76e53693e21a3f80a3c9c99774e67fd26
zeddic <zjf103552@outlook.com>
2024-05-23 18:44:47 +0800

    feat:Api test

--- src/main/java/com/zeddic/util/SignUtils.java
+++ src/main/java/com/zeddic/util/SignUtils.java
@@ -5,7 +5,6 @@ import org.apache.commons.lang3.StringUtils;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
 import java.util.TreeMap;
@@ -15,12 +14,12 @@ public class SignUtils {
     public static String createSign(Map < String, String > params, String partnerKey) {
         // 生成签名前先去除sign
         params.remove("sign");
-        String stringA = packageSign(params, false);
+        String stringA = packageSign(params);
         String stringSignTemp = stringA + "&key=" + partnerKey;
         return DigestUtils.md5Hex(stringSignTemp).toUpperCase();
     }
 
-    private static String packageSign(Map< String, String > params, boolean urlEncoder) {
+    private static String packageSign(Map< String, String > params) {
         // 先将参数以其参数名的字典序升序进行排序
         TreeMap< String, String > sortedParams = new TreeMap < String, String > (params);
         // 遍历排序后的字典，将所有参数按"key=value"格式拼接在一起
@@ -37,13 +36,6 @@ public class SignUtils {
                 sb.append("&");
             }
             sb.append(param.getKey()).append("=");
-            if (urlEncoder) {
-                try {
-                    value = urlEncode(value);
-                } catch (UnsupportedEncodingException e) {
-                    throw new RuntimeException(e);
-                }
-            }
             sb.append(value);
         }
         return sb.toString();
